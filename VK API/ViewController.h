ca1dd035d89b8ea953653a7a647683ee66cbbfd0
Vlad <vlad@macbook-pro-vlad.local>
2017-01-30 14:13:48 +0200

    Initial Commit

---
+++ VK API/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VK API
+//
+//  Created by Vlad on 30.01.17.
+//  Copyright © 2017 Vlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VK API/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VK API
+//
+//  Created by Vlad on 30.01.17.
+//  Copyright © 2017 Vlad. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
